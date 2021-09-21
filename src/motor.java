b7f3ff75c30b6d01480171c035ec17bac709b8be
LaChappp <chapurlat.adrien@gmail.com>
2021-09-21 10:34:08 +0200

    Création class motor

---
+++ src/motor.java
@@ -0,0 +1,13 @@
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+
+
+public class motor{
+	
+	
+}
