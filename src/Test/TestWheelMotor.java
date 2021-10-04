d9d8c5e34dd7b57c14299f6e8926fe550dcc62fb
martintroup <61577254+martintroup@users.noreply.github.com>
2021-10-04 15:10:27 +0200

    changement arcDeCercle et testWheelMotor

--- src/Test/TestWheelMotor.java
+++ src/Test/TestWheelMotor.java
@@ -1,10 +1,22 @@
 package Test;
 
+import java.util.Scanner;
+
+import Robot.WheelMotor;
+
 public class TestWheelMotor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+			Scanner sc = new Scanner(System.in);
+			System.out.println("1, 2 ou 3");
+			int num = sc.nextInt();
+			sc.close();
+			System.out.println(num);
+			WheelMotor m = new WheelMotor();
+			m.forward(10000);
+			m.arcDeCercle(1000,true);	
+			m.forward(3000);
 	}
 
 }
