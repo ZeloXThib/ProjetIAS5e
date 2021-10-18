8bbcca1b34809ce3803ab27df63cade0d609906f
martintroup <61577254+martintroup@users.noreply.github.com>
2021-10-18 16:03:38 +0200

    wheelmotor forward et backward avec boussole et coo

--- src/Test/TestWheelMotor.java
+++ src/Test/TestWheelMotor.java
@@ -2,20 +2,35 @@ package Test;
 
 
 import java.util.Scanner;
-
-import Moteur.Pinces;
 import Moteur.WheelMotor;
 import lejos.hardware.Button;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.robotics.Color;
+import lejos.robotics.navigation.MovePilot;
 import lejos.utility.Delay;
 import perception.Sensor;
 
+/**
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.navigation.MovePilot.*;
+*/
 public class TestWheelMotor {
 
 	public static void main(String[] args) {
-
-			WheelMotor m = new WheelMotor();
-			m.forward(2000);
+		
+		/**
+		
+		EV3LargeRegulatedMotor B = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor C = new EV3LargeRegulatedMotor(MotorPort.C);
+		
+		B.rotate(720,true);
+		C.rotate(720,false);
+			
+*/
+		
+		WheelMotor m1 = new WheelMotor();
+		m1.backward(200);
 
 			
 			
